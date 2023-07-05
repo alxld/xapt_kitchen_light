c12d50de0045f29759f8871797f8bbd63c15769a
alxld <aaronandlouise@yahoo.com>
2023-07-05 01:51:39 +0000

    Initial checking

---
+++ custom_components/xapt_kitchen_light/const.py
@@ -0,0 +1,3 @@
+"""Constants for the xapt_kitchen_light integration."""
+
+DOMAIN = "xapt_kitchen_light"
---
+++ custom_components/xapt_kitchen_light/light.py
@@ -0,0 +1,43 @@
+"""Platform for light integration"""
+from __future__ import annotations
+import sys
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+from . import DOMAIN
+
+sys.path.append("custom_components/new_light")
+from new_light import NewLight
+
+
+async def async_setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
+    """Set up the light platform."""
+    # We only want this platform to be set up via discovery.
+    if discovery_info is None:
+        return
+    ent = XaptKitchenLight()
+    add_entities([ent])
+
+
+class XaptKitchenLight(NewLight):
+    """Xapt Kitchen Light."""
+
+    def __init__(self) -> None:
+        """Initialize Xapt Kitchen Light."""
+        super(XaptKitchenLight, self).__init__(
+            "Xapt Kitchen Light", domain=DOMAIN, debug=False, debug_rl=False
+        )
+
+        self.entities["light.xapt_kitchen_group"] = None
+        #self.switch = "Xapt Kitchen Switch"
+        #self.motion_sensors.append("Dining Room Motion Sensor")
+
+        self.has_brightness_threshold = True
+        self.brightness_threshold = 191
+        self.motion_sensor_brightness = 128
